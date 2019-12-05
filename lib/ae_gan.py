6f3306bd5960bfff2540ec52e77f304c2d23c0a9
João Marcos Cardoso da Silva <joaomarcoscsilva@outlook.com>
2019-12-05 17:04:04 -0300

    Corrects wandb metrics names and makes train step functions return numpy arrays instead of eager tensors

--- lib/ae_gan.py
+++ lib/ae_gan.py
@@ -18,7 +18,7 @@ class Autoencoder():
             
         gradients = tape.gradient(loss, self.AE.Model.trainable_variables)
         self.AE.Optimizer.apply_gradients(zip(gradients, self.AE.Model.trainable_variables))
-        return loss, gradients, loss_reconstruction, loss_discrimination
+        return loss.numpy(), np.array(gradients), loss_reconstruction.numpy(), loss_discrimination.numpy()
 
     def train_step_Disc(self, batch):
         with tf.GradientTape() as tape:
@@ -31,7 +31,7 @@ class Autoencoder():
             loss = loss_real + loss_fake
         gradients = tape.gradient(loss, self.Disc.Discriminator.trainable_variables)
         self.Disc.Optimizer.apply_gradients(zip(gradients, self.Disc.Discriminator.trainable_variables))
-        return loss, gradients, loss_real, loss_fake
+        return loss.numpy(), np.array(gradients), loss_real.numpy(), loss_fake.numpy()
     
     def train(self, epochs, dataset, verbose = True, wandb_run = False):
         for epoch in range(epochs):
@@ -40,15 +40,18 @@ class Autoencoder():
             for batch in dataset:
                 loss_ae, gradients_ae, loss_reconstruction_ae, loss_discrimination_ae = self.train_step_AE(batch)
                 loss_disc, gradients_disc, loss_real_disc, loss_fake_disc = self.train_step_Disc(batch)
+                
                 if wandb_run:
                     wandb.log({'Epoch': epoch}, commit = False)
                     wandb.log({'Autoencoder Loss': loss_ae, 
-                        'Autoencoder Mean Gradient': gradients_ae, 
+                        'Autoencoder Mean Gradient': np.mean([np.mean(i) for i in gradients_ae]), 
                         'Autoencoder Reconstruction Loss': loss_reconstruction_ae,
                         'Autoencoder Discrimination Loss': loss_discrimination_ae}, commit = False)
+                
                     wandb.log({'Discriminator Loss': loss_disc, 
-                        'Discriminator Mean Gradient': gradients_disc, 
-                        'Autoencoder Reconstruction Loss': loss_real_disc,
-                        'Autoencoder Discrimination Loss': loss_fake_disc})
+                        'Discriminator Mean Gradient': np.mean([np.mean(i) for i in gradients_disc]),
+                        'Discriminator Real Loss': loss_real_disc,
+                        'Discriminator Fake Loss': loss_fake_disc})
+                  
                 if verbose == 1:
                     ...
