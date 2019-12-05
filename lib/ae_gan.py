f12e11855e18904b90b577f1a827fc563df96e44
João Marcos Cardoso da Silva <joaomarcoscsilva@outlook.com>
2019-12-05 16:20:21 -0300

    Makes ae_gan train steps return loss values and upload them to wandb

--- lib/ae_gan.py
+++ lib/ae_gan.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import wandb
 
 class Autoencoder():
     def __init__(self, autoencoder_object, discriminator_object, Lambda):
@@ -11,11 +12,13 @@ class Autoencoder():
             x_true = batch
             x_pred = self.AE.autoencode(batch)
             disc_pred = self.Disc.discriminate(x_pred)
-            loss = self.Lambda*self.AE.loss(batch, x_pred) + self.Disc.loss(tf.ones(tf.shape(disc_pred)), disc_pred)
+            loss_reconstruction = self.AE.loss(batch, x_pred) 
+            loss_discrimination = self.Disc.loss(tf.ones(tf.shape(disc_pred)), disc_pred)
+            loss = self.Lambda*loss_reconstruction + loss_discrimination
             
         gradients = tape.gradient(loss, self.AE.Model.trainable_variables)
         self.AE.Optimizer.apply_gradients(zip(gradients, self.AE.Model.trainable_variables))
-        return loss
+        return loss, gradients, loss_reconstruction, loss_discrimination
 
     def train_step_Disc(self, batch):
         with tf.GradientTape() as tape:
@@ -28,10 +31,23 @@ class Autoencoder():
             loss = loss_real + loss_fake
         gradients = tape.gradient(loss, self.Disc.Discriminator.trainable_variables)
         self.Disc.Optimizer.apply_gradients(zip(gradients, self.Disc.Discriminator.trainable_variables))
-        return loss
+        return loss, gradients, loss_real, loss_fake
     
-    def train(self, epochs, dataset):
+    def train(self, epochs, dataset, verbose = True, wandb_run = None):
         for epoch in range(epochs):
+            if verbose:
+                print('Epoch:',epoch+1)
             for batch in dataset:
-                self.train_step_AE(batch)
-                self.train_step_Disc(batch)
+                loss_ae, gradients_ae, loss_reconstruction_ae, loss_discrimination_ae = self.train_step_AE(batch)
+                loss_disc, gradients_disc, loss_real_disc, loss_fake_disc = self.train_step_Disc(batch)
+                if wandb_run != None:
+                    wandb.log({'Epoch': epoch}, commit = False)
+                    wandb.log({'Autoencoder Loss': loss_ae, 
+                        'Autoencoder Mean Gradient': gradients_ae, 
+                        'Autoencoder Reconstruction Loss': loss_reconstruction_ae,
+                        'Autoencoder Discrimination Loss': loss_discrimination_ae}, commit = False)
+                    wandb.log({'Discriminator Loss': loss_disc, 
+                        'Discriminator Mean Gradient': gradients_disc, 
+                        'Autoencoder Reconstruction Loss': loss_real_disc,
+                        'Autoencoder Discrimination Loss': loss_fake_disc}, commit = False)
+                
