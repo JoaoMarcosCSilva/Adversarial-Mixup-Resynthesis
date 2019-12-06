f0794c71373a94f2b630374ab772d35aa8ca27ca
unknown <joaomarcoscsilva@outlook.com>
2019-12-05 22:35:21 -0300

    Adds wandb logging to baseline model

--- lib/baseline.py
+++ lib/baseline.py
@@ -10,9 +10,15 @@ class Autoencoder():
             loss = self.AE.loss(batch, self.AE.autoencode(batch))
         gradients = tape.gradient(loss, self.AE.Model.trainable_variables)
         self.AE.Optimizer.apply_gradients(zip(gradients, self.AE.Model.trainable_variables))
-        return loss
+        return loss.numpy(), np.array(gradients)
 
-    def train(self, epochs, dataset):
+    def train(self, epochs, dataset, verbose = True, wandb_run = False):
         for epoch in range(epochs):
+            if verbose:
+                print('Epoch:',epoch+1)
             for batch in dataset:
-                self.train_step_AE(batch)
+                loss, gradients = self.train_step_AE(batch)
+            if wandb_run:
+                wandb.log({'Epoch': epoch}, commit = False)
+                wandb.log({'Autoencoder Loss': loss, 
+                    'Autoencoder Mean Gradient': np.mean([np.mean(i) for i in gradients]))
