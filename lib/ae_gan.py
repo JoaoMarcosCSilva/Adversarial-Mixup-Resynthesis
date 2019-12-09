581b09f4d2e60e96c554fa483c59a3872e0ee674
unknown <joaomarcoscsilva@outlook.com>
2019-12-09 15:35:28 -0300

    Completes subclassing for ae_gan from baseline

--- lib/ae_gan.py
+++ lib/ae_gan.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
-import wandb
 import numpy as np
 
-class Autoencoder(Base):
-    def __init__(self, Encoder, Decoder, Discriminator, Autoencoder_Optimizer = None, Discriminator_Optimizer = None, Lambda):
+from lib import baseline
+
+class Autoencoder(baseline.Autoencoder):
+    def __init__(self, Encoder, Decoder, Discriminator, Lambda, Autoencoder_Optimizer = None, Discriminator_Optimizer = None):
         super().__init__(Encoder, Decoder, Discriminator, Autoencoder_Optimizer, Discriminator_Optimizer)
         self.Lambda = Lambda
 
@@ -11,53 +12,58 @@ class Autoencoder(Base):
     def autoencoder_train_step(self, batch):
         with tf.GradientTape() as tape:
             x_true = batch
-            x_pred = self.AE.autoencode(batch)
-            disc_pred = self.Disc.discriminate(x_pred)
-            loss_reconstruction = self.AE.loss(batch, x_pred) 
-            loss_discrimination = self.Disc.loss(tf.ones(tf.shape(disc_pred)), disc_pred)
+            x_pred = self.Autoencoder(batch)
+            disc_pred = self.Discriminator(x_pred)
+            loss_reconstruction = self.autoencoder_loss(batch, x_pred) 
+            loss_discrimination = self.discriminator_loss(tf.ones(tf.shape(disc_pred)), disc_pred)
             loss = self.Lambda*loss_reconstruction + loss_discrimination
             
-        gradients = tape.gradient(loss, self.AE.Model.trainable_variables)
-        self.AE.Optimizer.apply_gradients(zip(gradients, self.AE.Model.trainable_variables))
+        gradients = tape.gradient(loss, self.Autoencoder.trainable_variables)
+        self.Autoencoder_Optimizer.apply_gradients(zip(gradients, self.Autoencoder.trainable_variables))
         return loss, gradients, loss_reconstruction, loss_discrimination
 
     @tf.function
     def discriminator_train_step(self, batch):
         with tf.GradientTape() as tape:
-            disc_pred_real = self.Disc.discriminate(batch)
-            disc_pred_fake = self.Disc.discriminate(self.AE.autoencode(batch))
+            disc_pred_real = self.Discriminator(batch)
+            disc_pred_fake = self.Discriminator(self.Autoencoder(batch))
             
-            loss_real = self.Disc.loss(tf.ones(tf.shape(disc_pred_real)), disc_pred_real)
-            loss_fake = self.Disc.loss(tf.zeros(tf.shape(disc_pred_fake)), disc_pred_fake)
+            loss_real = self.discriminator_loss(tf.ones(tf.shape(disc_pred_real)), disc_pred_real)
+            loss_fake = self.discriminator_loss(tf.zeros(tf.shape(disc_pred_fake)), disc_pred_fake)
 
             loss = loss_real + loss_fake
-        gradients = tape.gradient(loss, self.Disc.Discriminator.trainable_variables)
-        self.Disc.Optimizer.apply_gradients(zip(gradients, self.Disc.Discriminator.trainable_variables))
+        gradients = tape.gradient(loss, self.Discriminator.trainable_variables)
+        self.Discriminator_Optimizer.apply_gradients(zip(gradients, self.Discriminator.trainable_variables))
         return loss, gradients, loss_real, loss_fake
     
     def train(self, epochs, dataset, verbose = 1, log_wandb = 0, wandb_every = 1, disc_every = 1):
+        if log_wandb:
+            import wandb
         for epoch in range(epochs):
             if verbose:
                 print('Epoch:',epoch+1)
-            
+                progress_bar = self.get_progress_bar(dataset)
             for step, batch in enumerate(dataset):
-                loss_ae, gradients_ae, loss_reconstruction_ae, loss_discrimination_ae = self.train_step_AE(batch)
+                loss_ae, gradients_ae, loss_reconstruction_ae, loss_discrimination_ae = self.autoencoder_train_step(batch)
                 
                 if step % disc_every == 0:
-                    loss_disc, gradients_disc, loss_real_disc, loss_fake_disc = self.train_step_Disc(batch)
+                    loss_disc, gradients_disc, loss_real_disc, loss_fake_disc = self.discriminator_train_step(batch)
                 
-                if wandb_run:
-                    if step % wandb_every == 0:
-                        wandb.log({'Autoencoder Loss': loss_ae.numpy(), 
+                metrics_dict = {'Autoencoder Loss': loss_ae.numpy(), 
                             'Autoencoder Mean Gradient': np.mean([np.mean(i.numpy()) for i in gradients_ae]), 
                             'Autoencoder Reconstruction Loss': loss_reconstruction_ae.numpy(),
-                            'Autoencoder Discrimination Loss': loss_discrimination_ae.numpy()}, commit = False)
-                        
-                        wandb.log({'Discriminator Loss': loss_disc.numpy(), 
+                            'Autoencoder Discrimination Loss': loss_discrimination_ae.numpy(),
+                            'Discriminator Loss': loss_disc.numpy(), 
                             'Discriminator Mean Gradient': np.mean([np.mean(i.numpy()) for i in gradients_disc]),
                             'Discriminator Real Loss': loss_real_disc.numpy(),
-                            'Discriminator Fake Loss': loss_fake_disc.numpy()}, commit = False)
+                            'Discriminator Fake Loss': loss_fake_disc.numpy()}
 
-                        wandb.log({'Epoch': epoch})
+                if log_wandb:
+                    if step % wandb_every == 0:
+                        wandb_step(metrics_dict, epoch)
+
                 if verbose == 1:
-                    ...
+                    self.progress_bar_step(progress_bar, step, metrics_dict, ['Autoencoder Loss','Discriminator Loss', 'Autoencoder Reconstruction Loss'])
+                if verbose == 2:
+                    self.progress_bar_step(progress_bar, step, metrics_dict)
+                
--- lib/baseline.py
+++ lib/baseline.py
@@ -1,16 +1,20 @@
 import tensorflow as tf
 import numpy as np
-import wandb
+from tensorflow import keras
 
-class Base():
+from lib import losses
+class Autoencoder():
     def __init__(self, Encoder, Decoder, Discriminator = None, Autoencoder_Optimizer = None, Discriminator_Optimizer = None):
         self.Encoder = Encoder
         self.Decoder = Decoder
         self.Discriminator = Discriminator
-        self.Autoencoder = keras.Sequential(Encoder, Decoder)
+
+        inputs = keras.layers.Input(shape = Encoder.input_shape[1:])
+        self.Autoencoder = keras.Model(inputs, Decoder(Encoder(inputs)))
+
         self.Autoencoder_Optimizer = Autoencoder_Optimizer if Autoencoder_Optimizer != None else keras.optimizers.Adam()
         self.Discriminator_Optimizer = Discriminator_Optimizer if Discriminator_Optimizer != None else keras.optimizers.Adam()
-        
+
     def autoencoder_loss(self, x_true, x_pred):
         return losses.reconstruction_loss(x_true, x_pred)
     
@@ -26,9 +30,10 @@ class Base():
     @tf.function
     def autoencoder_train_step(self, batch):
         with tf.GradientTape() as tape:
-            loss = self.AE.loss(batch, self.AE.autoencode(batch))
-        gradients = tape.gradient(loss, self.AE.Model.trainable_variables)
-        self.AE.Optimizer.apply_gradients(zip(gradients, self.AE.Model.trainable_variables))
+            pred = self.Autoencoder(batch)
+            loss = self.autoencoder_loss(batch, pred)
+        gradients = tape.gradient(loss, self.Autoencoder.trainable_variables)
+        self.Autoencoder_Optimizer.apply_gradients(zip(gradients, self.Autoencoder.trainable_variables))
         return loss, gradients
     
     @tf.function
@@ -36,26 +41,39 @@ class Base():
         # The base autoencoder's does not have a discriminator, so this function does nothing
         return 
     
-    def wandb_step(self, dict):
-        wandb.log(dict)
+    def wandb_step(self, metrics_dict, epoch = None):
+        if epoch == None:
+            wandb.log(metrics_dict)
+        else:
+            wandb.log({**{'Epoch':epoch+1}, **metrics_dict})
     
-    def progress_bar_step(self, progress_bar, step, dict):
-        progress_bar.update(step, list(zip(dict.keys(), dict.values())))
+    def get_progress_bar(self, dataset):
+        return keras.utils.Progbar(int(tf.data.experimental.cardinality(dataset)) - 1)
+
+    def progress_bar_step(self, progress_bar, step, dict, subset = None):
+        if type(subset) is not list:
+            progress_bar.update(step, list(zip(dict.keys(), dict.values())))
+        else:
+            self.progress_bar_step(progress_bar, step, {k:dict[k] for k in dict if k in subset})
+            
     
     def train(self, epochs, dataset, verbose = 1, log_wandb = 0):
+        if log_wandb:
+            import wandb
         for epoch in range(epochs):
             if verbose:
                 print('Epoch:',epoch+1)
-                progress_bar = keras.utils.Progbar()
+                progress_bar = self.get_progress_bar(dataset)
                 
             for step, batch in enumerate(dataset):
-                autoencoder_loss, autoencoder_gradients = autoencoder_train_step(batch)
+                p = self.Autoencoder(batch)
+                autoencoder_loss, autoencoder_gradients = self.autoencoder_train_step(batch)
                 
                 metrics_dict = {'Autoencoder Reconstruction Loss':autoencoder_loss.numpy(),
-                                'Autoencoder Mean Gradient':np.mean([np.mean(i.numpy()) for i in gradients])}
+                                'Autoencoder Mean Gradient':np.mean([np.mean(i.numpy()) for i in autoencoder_gradients])}
                 if log_wandb:
-                    wandb_step({**{'Epoch':epoch+1}, **metrics_dict})
+                    wandb_step(metrics_dict, epoch)
                 
                 if verbose:
-                    progress_bar_step(progress_bar, step, metrics_dict)
+                    self.progress_bar_step(progress_bar, step, metrics_dict)
                     
