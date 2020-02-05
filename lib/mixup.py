6bc393b41d928ccc1c335a5dd9376b9dafba8811
João Marcos Cardoso da Silva <joaomarcoscsilva@outlook.com>
2020-02-05 09:36:25 -0300

    Adds linear interpolation function to mixup.py

--- lib/mixup.py
+++ lib/mixup.py
@@ -1,2 +1,9 @@
 def interpolate(code_a, code_b, t):
   return code_a*(1-t) + (t)*code_b
+
+def linear(codes, t = None):
+  length = tf.shape(codes)[0]
+  if t is None:
+    t = tf.random.uniform((length,1,1,1), 0, 0.5)
+  return interpolate(codes, codes[::-1], t), t
+  
